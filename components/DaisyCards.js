de8193e7f16e3ee6760af2cb819348984e9e4063
unknown <mhassankhanw3@gmail.com>
2022-11-10 00:59:23 -0700

    arrow

--- components/DaisyCards.js
+++ components/DaisyCards.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FiArrowLeft } from "react-icons/fi";
+import { FiArrowRight } from "react-icons/fi";
 import right_arrow from "../public/images/right-arrow.png";
 import left_arrow from "../public/images/left-arrow.png";
 import cstar_one from "../public/images/cstar1.png";
@@ -11,9 +13,15 @@ const DaisyCards = () => {
     <div>
       <div className={styles.daisycards}>
         <div className={styles.arrow}>
-          <Image className={styles.mainarrow} src={left_arrow} />
+          {/* <Image className={styles.mainarrow} src={left_arrow} /> */}
+          <div className={styles.radius}>
+            <FiArrowLeft className={styles.main_arrow} />
+          </div>
           <h1>Listen to our users talk about TopicMojo</h1>
-          <Image className={styles.mainarrow} src={right_arrow} />
+          <div className={styles.radius}>
+            <FiArrowRight className={styles.main_arrow} />
+          </div>
+          {/* <Image className={styles.mainarrow} src={right_arrow} /> */}
         </div>
         <div className={styles.allcards}>
           <div className={styles.card_one}>
@@ -24,14 +32,14 @@ const DaisyCards = () => {
               Pellentesque et placerat metus. Morbi aliquet felis sit amet.
             </p>
             <div className={styles.flex}>
-                <Image src={man}/>
-                <div className={styles.block_one}>
+              <Image src={man} />
+              <div className={styles.block_one}>
                 <h4>Daisy Carter</h4>
                 <p>Product Development</p>
-                </div>
+              </div>
             </div>
           </div>
-          
+
           <div className={styles.card_three}>
             <Image src={cstar_one} />
             <p className={styles.p_one}>
@@ -40,27 +48,31 @@ const DaisyCards = () => {
               Pellentesque et placerat metus. Morbi aliquet felis sit amet.
             </p>
             <div className={styles.flex}>
-                <Image src={man}/>
-                <div className={styles.block_three}>
+              <Image src={man} />
+              <div className={styles.block_three}>
                 <h4>Daisy Carter</h4>
                 <p>Product Development</p>
-                </div>
+              </div>
             </div>
           </div>
           <div className={styles.card_two}>
             <Image src={cstar_two} />
             <p className={styles.p_one}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet.  Pellentesque et placerat metus. Morbi aliquet felis sit amet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque et placerat metus. Morbi aliquet felis sit amet.
+              Pellentesque et placerat metus. Morbi aliquet felis sit amet.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit.
             </p>
             <div className={styles.flex}>
-                <Image src={man}/>
-                <div className={styles.block_two}>
+              <Image src={man} />
+              <div className={styles.block_two}>
                 <h4>Daisy Carter</h4>
                 <p>Product Development</p>
-                </div>
+              </div>
             </div>
           </div>
-          
+
           <div className={styles.card_five}>
             <Image src={cstar_one} />
             <p className={styles.p_one}>
@@ -69,24 +81,28 @@ const DaisyCards = () => {
               Pellentesque et placerat metus. Morbi aliquet felis sit amet.
             </p>
             <div className={styles.flex}>
-                <Image src={man}/>
-                <div className={styles.block_five}>
+              <Image src={man} />
+              <div className={styles.block_five}>
                 <h4>Daisy Carter</h4>
                 <p>Product Development</p>
-                </div>
+              </div>
             </div>
           </div>
           <div className={styles.card_four}>
             <Image src={cstar_two} />
             <p className={styles.p_one}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet.  Pellentesque et placerat metus. Morbi aliquet felis sit amet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque et placerat metus. Morbi aliquet felis sit amet.
+              Pellentesque et placerat metus. Morbi aliquet felis sit amet.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit.
             </p>
             <div className={styles.flex}>
-                <Image src={man}/>
-                <div className={styles.block_four}>
+              <Image src={man} />
+              <div className={styles.block_four}>
                 <h4>Daisy Carter</h4>
                 <p>Product Development</p>
-                </div>
+              </div>
             </div>
           </div>
         </div>
