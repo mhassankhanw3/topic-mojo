c8e9509e75b43beb151aaf9af57d6fe1d433e64e
unknown <mhassankhanw3@gmail.com>
2022-11-10 00:32:47 -0700

    animations

--- components/Content.js
+++ components/Content.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import back_three from "../public/images/bg3.png";
 import bulb from "../public/images/bulb.png";
 import styles from "../styles/DaisyCards.module.css";
@@ -6,19 +7,19 @@ import styles from "../styles/DaisyCards.module.css";
 const Content = () => {
   return (
     <div>
-        <div className={styles.mask}>
-            <Image className={styles.bg} src={back_three} />
+      <div className={styles.mask}>
+        <Image className={styles.bg} src={back_three} />
+      </div>
+      <div className={styles.bulb}>
+        <Image className={styles.bulb_img} src={bulb} />
+        <h2>Running out of content ideas?</h2>
+        <p>Fear not, we have the perfect solution for you.</p>
+        <div className={styles.shadow_three}>
+          <button className={styles.button}>Get Started at $10/mo</button>
         </div>
-        <div className={styles.bulb}>
-            <Image className={styles.bulb_img} src={bulb} />
-            <h2>Running out of content ideas?</h2>
-            <p>Fear not, we have the perfect solution for you.</p>
-            <div className={styles.shadow_three}>
-            <button className={styles.button}>Get Started at $10/mo</button>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
