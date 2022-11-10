08702c5a7bd2a1851322c103679b9731af6808b4
unknown <mhassankhanw3@gmail.com>
2022-11-09 21:54:29 -0700

    button shadow

--- components/LaunchOption.js
+++ components/LaunchOption.js
@@ -14,7 +14,9 @@ const LaunchOption = () => {
             <div className={styles.flex_launch}>
              <Image className={styles.launch_img} src={launch}/>
              <h2>Start your easy way out today with TopicMojo along with data that never upsets.</h2>
+             <div className={styles.shadow_two}>
              <button className={styles.button}>Get Started at $10/mo</button>
+             </div>
             </div>
         </div>
     </div>
